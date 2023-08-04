5b89febe9be9721ce38515434271f3a69e261054
Kemal Dahha <kemal-nl@hotmail.com>
2023-08-04 08:48:51 +0200

    initial commit

---
+++ timer.py
@@ -0,0 +1,21 @@
+from plyer import notification
+import time
+
+
+def start_timer(minutes):
+    t_start = time.time()
+    while True:
+        time.sleep(1)
+        seconds_elapsed = int(time.time() - t_start)
+        print(
+            f"{minutes - (seconds_elapsed // 60 + 1):02.0f}:{60-seconds_elapsed}",
+            end="\r",
+        )
+        if (seconds_elapsed > minutes * 60) or (seconds_elapsed == 10):
+            break
+    notification.notify(message="time's up!", timeout=5)
+
+
+if __name__ == "__main__":
+    limit_minutes = int(input("minutes: "))
+    start_timer(limit_minutes)
